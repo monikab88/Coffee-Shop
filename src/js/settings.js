1e06e6363aa62219eab712b67e05ac0e91f6121d
Monika Bielicz <bieliczm@gmail.com>
2023-07-16 18:23:29 +0200

    server2

--- src/js/settings.js
+++ src/js/settings.js
@@ -13,7 +13,7 @@ export const select = {
   
 export const settings = {
   db: {
-    url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     products: 'products',
   },
 };
